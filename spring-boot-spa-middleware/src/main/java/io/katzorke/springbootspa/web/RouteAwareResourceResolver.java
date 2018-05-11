ea035bcf0a973036008abb76e4da3398fcc293e5
Christian Katzorke <c.katzorke@web.de>
2018-05-11 14:54:46 +0200

    add tests

--- spring-boot-spa-middleware/src/main/java/io/katzorke/springbootspa/web/RouteAwareResourceResolver.java
+++ spring-boot-spa-middleware/src/main/java/io/katzorke/springbootspa/web/RouteAwareResourceResolver.java
@@ -9,42 +9,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.resource.AbstractResourceResolver;
 import org.springframework.web.servlet.resource.ResourceResolverChain;
 
 public class RouteAwareResourceResolver extends AbstractResourceResolver {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RouteAwareResourceResolver.class);
-	
+
 	private static final String INDEX = "index.html";
-	
+
 	private final String spaRootLocation;
 
 	private Resource index;
-	
+
 	public RouteAwareResourceResolver(String spaLocation, ResourceLoader resourceLoader) {
 		this.spaRootLocation = spaLocation;
 		StringBuilder indexLocation = new StringBuilder(this.spaRootLocation);
-		if(!this.spaRootLocation.endsWith("/")) {
+		if (!this.spaRootLocation.endsWith("/")) {
 			indexLocation.append('/');
 		}
 		indexLocation.append(INDEX);
 		this.index = resourceLoader.getResource(indexLocation.toString());
 	}
-	
-	
 
 	@Override
 	protected Resource resolveResourceInternal(HttpServletRequest request, String requestPath,
 			List<? extends Resource> locations, ResourceResolverChain chain) {
 		LOG.debug("resolving path '{}'", requestPath);
+		return locateResource(requestPath, locations);
+	}
+
+	@Override
+	protected String resolveUrlPathInternal(String resourceUrlPath, List<? extends Resource> locations,
+			ResourceResolverChain chain) {
+		return (StringUtils.hasText(resourceUrlPath) && locateResource(resourceUrlPath, locations) != null
+				? resourceUrlPath
+				: null);
+	}
+
+	private Resource locateResource(String requestPath, List<? extends Resource> locations) {
 		for (Resource location : locations) {
 			LOG.debug("Looking in {}.", location);
 			Resource located = null;
 			try {
 				located = location.createRelative(requestPath);
 				LOG.debug("Located resource '{}' does exist? {}", located.getURI(), located.exists());
-				if(located.exists()) {
+				if (located.exists()) {
 					return located;
 				}
 			} catch (IOException e) {
@@ -54,12 +65,4 @@ public class RouteAwareResourceResolver extends AbstractResourceResolver {
 		LOG.debug("Resource with path {} was not found, return index.html", requestPath);
 		return index;
 	}
-
-	@Override
-	protected String resolveUrlPathInternal(String resourceUrlPath, List<? extends Resource> locations,
-			ResourceResolverChain chain) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- spring-boot-spa-middleware/src/test/java/io/katzorke/springbootspa/SpringBootSpaMiddlewareApplicationTests.java
+++ spring-boot-spa-middleware/src/test/java/io/katzorke/springbootspa/SpringBootSpaMiddlewareApplicationTests.java
@@ -1,16 +1,50 @@
 package io.katzorke.springbootspa;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
+@AutoConfigureMockMvc
 public class SpringBootSpaMiddlewareApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void redirectToIndex() throws Exception {
+		mvc.perform(get("/").accept(MediaType.ALL)).andExpect(status().is(HttpStatus.FOUND.value()))
+				.andExpect(redirectedUrl("/index.html"));
+	}
+
+	@Test
+	public void faviconIsResolved() throws Exception {
+		mvc.perform(get("/favicon.ico").accept(MediaType.ALL)).andExpect(status().isOk());
+	}
+
+	@Test
+	public void routeIsResolvedToIndex() throws Exception {
+		mvc.perform(get("/heroes").accept(MediaType.ALL)).andExpect(status().isOk())
+				.andExpect(content().string(containsString("My SPA")));
+	}
 }
